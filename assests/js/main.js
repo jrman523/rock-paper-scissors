0417334a2293a0fe7af79bfc0eb0820c7a880703
jad <jadelramadi@gmail.com>
2019-03-08 00:52:27 -0500

    pushing the last changes to style

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,21 +1,23 @@
 var config = {
-    apiKey: "AIzaSyAhflHnnanYXXB1DXrgNf8a0UojStFhjlA",
-    authDomain: "rock-papper-scissors-37aff.firebaseapp.com",
-    databaseURL: "https://rock-papper-scissors-37aff.firebaseio.com",
-    projectId: "rock-papper-scissors-37aff",
-    storageBucket: "rock-papper-scissors-37aff.appspot.com",
-    messagingSenderId: "682542883310"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyAhflHnnanYXXB1DXrgNf8a0UojStFhjlA",
+  authDomain: "rock-papper-scissors-37aff.firebaseapp.com",
+  databaseURL: "https://rock-papper-scissors-37aff.firebaseio.com",
+  projectId: "rock-papper-scissors-37aff",
+  storageBucket: "rock-papper-scissors-37aff.appspot.com",
+  messagingSenderId: "682542883310"
+};
+firebase.initializeApp(config);
 
 
 
-  $(document).on('click','#player-1', function(){
-    $('#player-1').addClass('disabled');
-    $('#player').text('player-1');
-  });
+$('#player-1').on('click', function () {
+  $(this).addClass('disabled');
+  $(this).addClass('btn-secondary');
+  $(this).removeClass('btn-outline-info');
+});
 
-  $(document).on('click','#player-2', function(){
-    $('#player-2').addClass('disabled');
-    $('#player').text('player-2');
-  });
+$('#player-2').on('click', function () {
+  $('#player-2').addClass('disabled');
+  $(this).addClass('btn-secondary');
+  $(this).removeClass('btn-outline-info');
+});
