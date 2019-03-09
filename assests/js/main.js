b12b1569b4215e90abc4012a8920da5979f9b98e
jad <jadelramadi@gmail.com>
2019-03-09 01:22:56 -0500

    pushing the main html and css

--- assests/js/main.js
+++ assests/js/main.js
@@ -8,16 +8,27 @@ var config = {
 };
 firebase.initializeApp(config);
 
+window.onload = function () {
+  $('#set-1').hide();
+  $('#set-2').hide();
+}
 
+$(document).on('click', 'img', function () {
+  var image = this.value;
+  console.log(image);
+});
 
 $('#player-1').on('click', function () {
   $(this).addClass('disabled');
   $(this).addClass('btn-secondary');
   $(this).removeClass('btn-outline-info');
+  $('#set-1').show();
 });
 
 $('#player-2').on('click', function () {
   $('#player-2').addClass('disabled');
   $(this).addClass('btn-secondary');
   $(this).removeClass('btn-outline-info');
+  $('#set-2').show();
 });
+
